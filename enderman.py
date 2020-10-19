0db6664b6b105548a8c61c9bddf3dd088200c36c
Jett-immer <22immerj@mbbc.qld.edu.au>
2020-10-19 11:46:10 +1000

    Week 3 lesson 1

---
+++ enderman.py
@@ -0,0 +1,13 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+b = (0,0,0)
+p = (191,5,247)
+w = (255,255,255)
+
+image_pixels = [
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b
