e8c50175f288e925c5db4427d8713dc742abf59d
Jett-immer <22immerj@mbbc.qld.edu.au>
2020-11-23 11:43:04 +1000

    last week

--- assessment\.py
+++ assessment\.py
@@ -32,7 +32,7 @@ while True:
     message = "Temp:" + str(temp) + " Pressure:" + str(press) + " Pitch:" + str(pitch) + " Roll:" + str(roll) + " Yaw:" + str(yaw)
     sense.show_message(message, scroll_speed = 0.10)
     #waits 30 seconds before reposting data
-    time.sleep(20)
+    time.sleep(10)
     
     
 
